a5ccdf74f11d3387b9a43720fb92c14825a13a7f
LaunchGemini <metabaseguy@proton.me>
2022-11-23 15:31:46 +0000

    Continue.

---
+++ Painter2/Form1.Designer.cs
@@ -0,0 +1,119 @@
+
+﻿namespace Painter2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel_DispImage = new System.Windows.Forms.Panel();
+            this.pictureBox_ImageShowForm = new System.Windows.Forms.PictureBox();
+            this.pictureBox_Dir2 = new System.Windows.Forms.PictureBox();
+            this.groupBox_ColorList = new System.Windows.Forms.GroupBox();
+            this.rbt_Color_changed = new System.Windows.Forms.RadioButton();
+            this.rbt_SetColor = new System.Windows.Forms.RadioButton();
+            this.panel_ColorList = new System.Windows.Forms.Panel();
+            this.button_ChangeColor = new System.Windows.Forms.Button();
+            this.checkBox_ChangeColor_All = new System.Windows.Forms.CheckBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cbx_Cursor = new System.Windows.Forms.ComboBox();
+            this.label_widthValue = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.trackBar_width = new System.Windows.Forms.TrackBar();
+            this.label_width_Max = new System.Windows.Forms.Label();
+            this.label_width_Min = new System.Windows.Forms.Label();
+            this.button_SetColor = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button_ClearAll = new System.Windows.Forms.Button();
+            this.label_Coordinate = new System.Windows.Forms.Label();
+            this.label_Coordinate_Cursor = new System.Windows.Forms.Label();
+            this.colorDialog_SetColor = new System.Windows.Forms.ColorDialog();
+            this.radioButton_OrigImg = new System.Windows.Forms.RadioButton();
+            this.radioButton_DrawImg = new System.Windows.Forms.RadioButton();
+            this.button_Clear = new System.Windows.Forms.Button();
+            this.label_Size = new System.Windows.Forms.Label();
+            this.label_Memory = new System.Windows.Forms.Label();
+            this.txt_GrayValue = new System.Windows.Forms.TextBox();
+            this.label20 = new System.Windows.Forms.Label();
+            this.txt_RGBValue = new System.Windows.Forms.TextBox();
+            this.label19 = new System.Windows.Forms.Label();
+            this.trackBar_zoom = new System.Windows.Forms.TrackBar();
+            this.label_zoom = new System.Windows.Forms.Label();
+            this.panel_Info = new System.Windows.Forms.Panel();
+            this.icon_Memory = new System.Windows.Forms.Button();
+            this.icon_ImageSize = new System.Windows.Forms.Button();
+            this.button_zoomOut = new System.Windows.Forms.Button();
+            this.icon_Coordinate = new System.Windows.Forms.Label();
+            this.button_zoomIn = new System.Windows.Forms.Button();
+            this.btnLoadImg = new System.Windows.Forms.Button();
+            this.radioButton_penPoint = new System.Windows.Forms.RadioButton();
+            this.radioButton_eraser = new System.Windows.Forms.RadioButton();
+            this.panel_Tool = new System.Windows.Forms.Panel();
+            this.radioButton_selectAnyShape = new System.Windows.Forms.RadioButton();
+            this.radioButton_selectRect = new System.Windows.Forms.RadioButton();
+            this.radioButton_ellipse_fill = new System.Windows.Forms.RadioButton();
+            this.radioButton_ellipse = new System.Windows.Forms.RadioButton();
+            this.radioButton_circle_fill = new System.Windows.Forms.RadioButton();
+            this.radioButton_circle = new System.Windows.Forms.RadioButton();
+            this.radioButton_rectangle_fill = new System.Windows.Forms.RadioButton();
+            this.radioButton_rectangle = new System.Windows.Forms.RadioButton();
+            this.imageList = new System.Windows.Forms.ImageList(this.components);
+            this.listViewImages = new System.Windows.Forms.ListView();
+            this.button_LoadBatchImages = new System.Windows.Forms.Button();
+            this.button_Save = new System.Windows.Forms.Button();
+            this.button_SavePath = new System.Windows.Forms.Button();
+            this.button_NextImage = new System.Windows.Forms.Button();
+            this.button_PreviousImage = new System.Windows.Forms.Button();
+            this.button_Recovery = new System.Windows.Forms.Button();
+            this.button_Redo = new System.Windows.Forms.Button();
+            this.panel_BatchImages = new System.Windows.Forms.Panel();
+            this.menuStrip = new System.Windows.Forms.MenuStrip();
+            this.檔案ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.結束ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.工具ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.儲存設定ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.語言設定ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.中文ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.英文ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.標註顏色批次轉換ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.說明ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.快捷鍵ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label_Version = new System.Windows.Forms.Label();
+            this.button_dispImg = new System.Windows.Forms.Button();
+            this.label_VersionNum = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cbx_LabelledColor = new System.Windows.Forms.ComboBox();
+            this.checkBox_Alpha = new System.Windows.Forms.CheckBox();
+            this.nud_Alpha = new System.Windows.Forms.NumericUpDown();
+            this.button_LoadBatchImages2 = new System.Windows.Forms.Button();
+            this.panel_Dir2 = new System.Windows.Forms.Panel();
+            this.button_Color_changed = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.panel_DispImage.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_ImageShowForm)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Dir2)).BeginInit();
