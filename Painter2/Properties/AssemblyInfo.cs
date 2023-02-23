20827929cf361349487f4a978b7ad671af05d38a
LaunchGemini <metabaseguy@proton.me>
2023-02-23 16:43:55 +0000

    Update horseland.html

--- Painter2/Properties/AssemblyInfo.cs
+++ Painter2/Properties/AssemblyInfo.cs
@@ -6,4 +6,9 @@ using System.Runtime.InteropServices;
 // 變更這些屬性的值即可修改組件的相關
 // 資訊。
 [assembly: AssemblyTitle("Painter4")]
-[assem
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Painter4")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: Asse
