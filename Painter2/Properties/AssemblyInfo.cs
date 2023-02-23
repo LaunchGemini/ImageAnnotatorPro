dcd8cf30bb56c1fca2781050b006f5976d6c5897
LaunchGemini <metabaseguy@proton.me>
2023-02-23 05:37:35 +0000

    Update ingroup.lua

--- Painter2/Properties/AssemblyInfo.cs
+++ Painter2/Properties/AssemblyInfo.cs
@@ -1,1 +1,9 @@
-﻿us
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// 組件的一般資訊是由下列的屬性集控制。
+// 變更這些屬性的值即可修改組件的相關
+// 資訊。
+[assembly: AssemblyTitle("Painter4")]
+[assem
