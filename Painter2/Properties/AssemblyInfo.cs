34ad4d807ae38c2b970c3a88eaa8e7298fa3b38c
LaunchGemini <metabaseguy@proton.me>
2023-03-01 10:13:21 +0000

    Update version string.

--- Painter2/Properties/AssemblyInfo.cs
+++ Painter2/Properties/AssemblyInfo.cs
@@ -11,4 +11,10 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Painter4")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
-[assembly: Asse
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// 將 ComVisible 設定為 false 會使得這個組件中的類型
+// 對 COM 元件而言為不可見。如果您需要從 COM 存取這個組件中
+// 的類型，請在該類型上將 ComVisible 屬性設定為 true。
+[assembly: ComVisible(false)
