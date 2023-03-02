87c103efc7b2b0a84109e803bbe3940a711e9017
LaunchGemini <metabaseguy@proton.me>
2023-03-02 20:31:54 +0000

    ALSA: pcm: Fix potential deadlock in OSS emulation

--- Painter2/Properties/AssemblyInfo.cs
+++ Painter2/Properties/AssemblyInfo.cs
@@ -28,4 +28,9 @@ using System.Runtime.InteropServices;
 //      次要版本 
 //      組建編號
 //      修訂編號
-/
+//
+// 您可以指定所有的值，也可以依照以下的方式，使用 '*' 將組建和修訂編號
+// 指定為預設值: 
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("3.0.2.0")]
+[assembly: AssemblyFileVersion("3.0.2.0")]
