edc0086ab812def0497d3693175979cf44041ede
LaunchGemini <metabaseguy@proton.me>
2023-03-02 15:13:47 +0000

    mingw: initialize HOME on startup

--- Painter2/Properties/AssemblyInfo.cs
+++ Painter2/Properties/AssemblyInfo.cs
@@ -17,4 +17,15 @@ using System.Runtime.InteropServices;
 // 將 ComVisible 設定為 false 會使得這個組件中的類型
 // 對 COM 元件而言為不可見。如果您需要從 COM 存取這個組件中
 // 的類型，請在該類型上將 ComVisible 屬性設定為 true。
-[assembly: ComVisible(false)
+[assembly: ComVisible(false)]
+
+// 下列 GUID 為專案公開 (Expose) 至 COM 時所要使用的 typelib ID
+[assembly: Guid("31ad6bec-ca96-4b16-9c42-270f29cdab37")]
+
+// 組件的版本資訊由下列四個值所組成: 
+//
+//      主要版本
+//      次要版本 
+//      組建編號
+//      修訂編號
+/
