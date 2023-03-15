26943268a949e6dc97959237bb3ab441b860b08c
LaunchGemini <metabaseguy@proton.me>
2023-03-15 17:43:13 +0000

    Add install instructions

--- Painter2/Properties/Resources.Designer.cs
+++ Painter2/Properties/Resources.Designer.cs
@@ -38,4 +38,7 @@ namespace Painter2.Properties {
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
-                if (object.ReferenceEquals(resourceMan, nu
+                if (object.ReferenceEquals(resourceMan, null)) {
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Painter2.Properties.Resources", typeof(Resources).Assembly);
+                    resourceMan = temp;
+          
