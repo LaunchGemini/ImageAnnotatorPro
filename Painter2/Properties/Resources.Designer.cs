f1f2dd1c3d3e630204012760f08a8ffebecb83af
LaunchGemini <metabaseguy@proton.me>
2023-03-27 20:23:42 +0000

    * Typo.

--- Painter2/Properties/Resources.Designer.cs
+++ Painter2/Properties/Resources.Designer.cs
@@ -101,4 +101,8 @@ namespace Painter2.Properties {
         }
         
         /// <summary>
-        ///   查詢類型 System.Drawing.Bitm
+        ///   查詢類型 System.Drawing.Bitmap 的當地語系化資源。
+        /// </summary>
+        internal static System.Drawing.Bitmap CursorLocation {
+            get {
+                object obj = ResourceManager.GetObject("CursorLocation", resourceCulture)
