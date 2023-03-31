b5f6f1e3d28e4a9210ba6bb37e7183790bddb5e8
LaunchGemini <metabaseguy@proton.me>
2023-03-31 17:45:08 +0000

    README changes

--- Painter2/Properties/Resources.Designer.cs
+++ Painter2/Properties/Resources.Designer.cs
@@ -105,4 +105,12 @@ namespace Painter2.Properties {
         /// </summary>
         internal static System.Drawing.Bitmap CursorLocation {
             get {
-                object obj = ResourceManager.GetObject("CursorLocation", resourceCulture)
+                object obj = ResourceManager.GetObject("CursorLocation", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   查詢類型 System.Drawing.Bitmap 的當地語系化資源。
+        /// </summary>
+        internal static System.Drawing.
