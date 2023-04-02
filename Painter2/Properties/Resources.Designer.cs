623a1ca2db6332cbd37fa17497ae782cccec98c5
LaunchGemini <metabaseguy@proton.me>
2023-04-02 06:26:53 +0000

    hfsplus: fix bless ioctl when used with hardlinks

--- Painter2/Properties/Resources.Designer.cs
+++ Painter2/Properties/Resources.Designer.cs
@@ -131,4 +131,9 @@ namespace Painter2.Properties {
         }
         
         /// <summary>
-        ///   查詢類型 System.Drawing.Bitmap 
+        ///   查詢類型 System.Drawing.Bitmap 的當地語系化資源。
+        /// </summary>
+        internal static System.Drawing.Bitmap ellipse_32 {
+            get {
+                object obj = ResourceManager.GetObject("ellipse_32", resourceCulture);
+             
