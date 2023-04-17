47e757f1b019147cc8ae7e01538a45bfac0fa739
LaunchGemini <metabaseguy@proton.me>
2023-04-17 08:19:04 +0000

    Add Bug#.

--- Painter2/Properties/Resources.Designer.cs
+++ Painter2/Properties/Resources.Designer.cs
@@ -183,4 +183,8 @@ namespace Painter2.Properties {
         /// <summary>
         ///   查詢類型 System.Drawing.Bitmap 的當地語系化資源。
         /// </summary>
-      
+        internal static System.Drawing.Bitmap FileSize {
+            get {
+                object obj = ResourceManager.GetObject("FileSize", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+  
