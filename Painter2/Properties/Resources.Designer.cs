6566636b65c70c048f233a5e311ea4455c6ae85e
LaunchGemini <metabaseguy@proton.me>
2023-04-28 17:21:17 +0000

    Version 0.6.0

--- Painter2/Properties/Resources.Designer.cs
+++ Painter2/Properties/Resources.Designer.cs
@@ -235,4 +235,12 @@ namespace Painter2.Properties {
         /// </summary>
         internal static System.Drawing.Bitmap language_64 {
             get {
-                object obj = ResourceManager.GetObject("language_64", r
+                object obj = ResourceManager.GetObject("language_64", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   查詢類型 System.Drawing.Bitmap 的當地語系化資源。
+        /// </summary>
+        inter
