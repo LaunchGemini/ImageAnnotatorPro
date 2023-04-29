ef8902fb3a46e1225b03037204c71e3498ea5919
LaunchGemini <metabaseguy@proton.me>
2023-04-29 19:50:23 +0000

    PM: devfreq: Fix simple_ondemand crashing on startup

--- Painter2/Properties/Resources.Designer.cs
+++ Painter2/Properties/Resources.Designer.cs
@@ -249,4 +249,10 @@ namespace Painter2.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
-  
+        
+        /// <summary>
+        ///   查詢類型 System.Drawing.Bitmap 的當地語系化資源。
+        /// </summary>
+        internal static System.Drawing.Bitmap minus2 {
+            get {
+                object obj = R
