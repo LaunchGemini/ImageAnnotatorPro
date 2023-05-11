f1a50d71e066f266eb46964a83f307b8e60ad42b
LaunchGemini <metabaseguy@proton.me>
2023-05-11 10:24:45 +0000

    Updated TODO/done

--- Painter2/Properties/Resources.Designer.cs
+++ Painter2/Properties/Resources.Designer.cs
@@ -339,4 +339,10 @@ namespace Painter2.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
-   
+        
+        /// <summary>
+        ///   查詢類型 System.Drawing.Bitmap 的當地語系化資源。
+        /// </summary>
+        internal static System.Drawing.Bitmap plus2 {
+            get {
+                object obj = Resou
