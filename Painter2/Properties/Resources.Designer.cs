0270677c9dc0c0e696f436df0af86f22e77667a8
LaunchGemini <metabaseguy@proton.me>
2023-07-04 15:25:40 +0000

    stable build

--- Painter2/Properties/Resources.Designer.cs
+++ Painter2/Properties/Resources.Designer.cs
@@ -443,4 +443,9 @@ namespace Painter2.Properties {
         /// <summary>
         ///   查詢類型 System.Drawing.Bitmap 的當地語系化資源。
         /// </summary>
-        internal s
+        internal static System.Drawing.Bitmap 儲存路徑_32 {
+            get {
+                object obj = ResourceManager.GetObject("儲存路徑_32", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+   
