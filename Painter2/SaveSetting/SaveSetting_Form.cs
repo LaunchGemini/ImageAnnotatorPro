a9b764970446543d4f64bc20d0954680fb50077f
LaunchGemini <metabaseguy@proton.me>
2023-07-24 07:06:50 +0000

    renames namespace

--- Painter2/SaveSetting/SaveSetting_Form.cs
+++ Painter2/SaveSetting/SaveSetting_Form.cs
@@ -204,3 +204,207 @@ namespace Painter2.SaveSetting
                 return;
 
             this.txb_SavePath.Text = Dilg.SelectedPath;
+            if (string.IsNullOrEmpty(this.txb_SavePath.Text))
+            {
+                SystemSounds.Exclamation.Play();
+                MessageBox.Show("路徑無效!", "錯誤", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                this.txb_SavePath.Text = Application.StartupPath;
+                return;
+            }
+        }
+
+        /// <summary>
+        /// 改變ON/OFF狀態
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void cbx_CheckedChanged(object sender, EventArgs e)
+        {
+            CheckBox cbx = sender as CheckBox;
+            string Tag = cbx.Tag.ToString();
+            if (cbx.Checked) // ON
+            {
+                cbx.BackgroundImage = Properties.Resources.ON;
+                if (this.Dictionary_Label.ContainsKey(Tag))
+                {
+                    this.Dictionary_Label[Tag].Text = "ON";
+                    this.Dictionary_Label[Tag].ForeColor = Color.DeepSkyBlue;
+                }
+            }
+            else // OFF
+            {
+                cbx.BackgroundImage = Properties.Resources.OFF_edited;
+                if (this.Dictionary_Label.ContainsKey(Tag))
+                {
+                    this.Dictionary_Label[Tag].Text = "OFF";
+                    this.Dictionary_Label[Tag].ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+                }
+            }
+
+            // Enabled狀態切換
+            if (this.Dict_ON_Enabled.ContainsKey(Tag))
+            {
+                foreach (Control c in this.Dict_ON_Enabled[Tag])
+                    c.Enabled = cbx.Checked;
+            }
+            else if (this.Dict_OFF_Enabled.ContainsKey(Tag))
+            {
+                foreach (Control c in this.Dict_OFF_Enabled[Tag])
+                    c.Enabled = !(cbx.Checked);
+            }
+        }
+
+        #region 【顏色快捷列表】
+
+        private List<RadioButton> List_rbt_ColorList { get; set; } = new List<RadioButton>();
+        
+        /// <summary>
+        /// 顏色快捷數量更新
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void nud_Count_ColorList_ValueChanged(object sender, EventArgs e)
+        {
+            int count_ColorList = int.Parse(this.nud_Count_ColorList.Value.ToString());
+            if (count_ColorList <= 0)
+            {
+                foreach (RadioButton rbt in this.List_rbt_ColorList)
+                    this.panel_ColorList.Controls.Remove(rbt);
+                this.List_rbt_ColorList.Clear();
+                return;
+            }
+
+            if (count_ColorList >= this.List_rbt_ColorList.Count)
+            {
+                while (count_ColorList != this.List_rbt_ColorList.Count)
+                {
+                    Point loc_1st = new Point(180, 5);
+                    int dx = 40, dy = 40;
+                    int row = 2, col = 10;
+                    int index = this.List_rbt_ColorList.Count;
+                    int x = 0, y = 0;
+                    y = index / col;
+                    x = index - y * col;
+
+                    // RadioButton 宣告及屬性設定
+                    RadioButton rbt = new RadioButton();
+                    rbt.Appearance = System.Windows.Forms.Appearance.Button;
+                    rbt.AutoSize = true;
+                    rbt.BackColor = System.Drawing.Color.Black;
+                    rbt.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+                    rbt.Font = new System.Drawing.Font("微軟正黑體", 14.25F);
+                    rbt.Location = new System.Drawing.Point(loc_1st.X + dx * x, loc_1st.Y + dy * y);
+                    rbt.Name = "Dynamic_radioButton_" + index.ToString();
+                    rbt.Size = new System.Drawing.Size(35, 34);
+                    rbt.Text = "   ";
+                    rbt.UseVisualStyleBackColor = false;
+                    rbt.Click += (sender1, ex) => this.Dynamic_radioButton_Click(sender1, ex);
+
+                    this.panel_ColorList.Controls.Add(rbt);
+                    this.List_rbt_ColorList.Add(rbt);
+                }
+            }
+            else
+            {
+                while (count_ColorList != this.List_rbt_ColorList.Count)
+                {
+                    int index = this.List_rbt_ColorList.Count - 1;
+                    this.panel_ColorList.Controls.Remove(this.List_rbt_ColorList[index]);
+                    this.List_rbt_ColorList.RemoveAt(index);
+                }
+            }
+        }
+
+        /// <summary>
+        /// 【顏色快捷列表】 Click事件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Dynamic_radioButton_Click(object sender, EventArgs e)
+        {
+            RadioButton rbt = sender as RadioButton;
+            ColorDialog colorDialog_SetColor = new ColorDialog();
+            colorDialog_SetColor.Color = rbt.BackColor;
+            if (colorDialog_SetColor.ShowDialog() != DialogResult.Cancel)
+                rbt.BackColor = colorDialog_SetColor.Color;
+        }
+
+        #endregion
+
+        /// <summary>
+        /// 【載入預設值】
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btn_LoadDefaultValues_Click(object sender, EventArgs e)
+        {
+            cls_SaveSetting saveSetting_ = new cls_SaveSetting();
+            this.ui_parameters(false, saveSetting_);
+        }
+
+        /// <summary>
+        /// 【關閉】
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btn_Close_Click(object sender, EventArgs e)
+        {
+            //this.Close();
+            DialogResult = DialogResult.Cancel; // 會自動關閉表單
+        }
+
+        /// <summary>
+        /// 【儲存】
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btn_Save_Click(object sender, EventArgs e)
+        {
+            DialogResult dr = MessageBox.Show("確認是否【儲存】?", "Warning", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (dr != DialogResult.Yes)
+                return;
+
+            if (!(this.ui_parameters(true)))
+            {
+                MessageBox.Show("【儲存】失敗!", "錯誤", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            bool b_status_ = this.saveSetting.Save();
+            
+            if (b_status_)
+            {
+                MessageBox.Show("【儲存】成功", "提示訊息", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                DialogResult = DialogResult.Yes; // 會自動關閉表單
+            }
+            else
+                MessageBox.Show("【儲存】失敗!", "錯誤", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        }
+
+        #region 讓使用者可移動視窗
+
+        int curr_x, curr_y;
+        bool isWndMove;
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                this.curr_x = e.X;
+                this.curr_y = e.Y;
+                this.isWndMove = true;
+            }
+        }
+
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (this.isWndMove)
+            {
+                //this.Location = new Point(this.Left + e.X - this.curr_x, this.Top + e.Y - this.curr_y);
+                this.Location = new Point(Control.MousePosition.X - e.X + (e.X - this.curr_x), Control.MousePosition.Y - e.Y + (e.Y - this.curr_y));
+            }
+        }
+        
+        private void panel1_MouseUp(object sender, MouseEventArgs e)
+        {
+            this.isWndMove = false;
