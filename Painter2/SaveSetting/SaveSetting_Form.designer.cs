64ac19cef165172bac52b399334dce8e73a9ac15
LaunchGemini <metabaseguy@proton.me>
2023-07-26 14:04:20 +0000

    Fixed some problems

--- Painter2/SaveSetting/SaveSetting_Form.designer.cs
+++ Painter2/SaveSetting/SaveSetting_Form.designer.cs
@@ -227,3 +227,201 @@
             this.lbl_Load_AllImageFormat.TabIndex = 103;
             this.lbl_Load_AllImageFormat.Text = "OFF";
             // 
+            // label_Load_AllImageFormat
+            // 
+            this.label_Load_AllImageFormat.AutoSize = true;
+            this.label_Load_AllImageFormat.Font = new System.Drawing.Font("微軟正黑體", 12F);
+            this.label_Load_AllImageFormat.Location = new System.Drawing.Point(10, 270);
+            this.label_Load_AllImageFormat.Name = "label_Load_AllImageFormat";
+            this.label_Load_AllImageFormat.Size = new System.Drawing.Size(201, 20);
+            this.label_Load_AllImageFormat.TabIndex = 102;
+            this.label_Load_AllImageFormat.Text = "載入所有類型影像檔案格式";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.panel1.Controls.Add(this.pictureBox_Icon);
+            this.panel1.Controls.Add(this.lbl_title);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(640, 43);
+            this.panel1.TabIndex = 107;
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            this.panel1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseUp);
+            // 
+            // pictureBox_Icon
+            // 
+            this.pictureBox_Icon.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox_Icon.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox_Icon.Image = global::Painter2.Properties.Resources.settings_32;
+            this.pictureBox_Icon.Location = new System.Drawing.Point(5, 5);
+            this.pictureBox_Icon.Name = "pictureBox_Icon";
+            this.pictureBox_Icon.Size = new System.Drawing.Size(32, 32);
+            this.pictureBox_Icon.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox_Icon.TabIndex = 238;
+            this.pictureBox_Icon.TabStop = false;
+            // 
+            // lbl_title
+            // 
+            this.lbl_title.AutoSize = true;
+            this.lbl_title.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_title.ForeColor = System.Drawing.Color.White;
+            this.lbl_title.Location = new System.Drawing.Point(40, 12);
+            this.lbl_title.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbl_title.Name = "lbl_title";
+            this.lbl_title.Size = new System.Drawing.Size(72, 18);
+            this.lbl_title.TabIndex = 1;
+            this.lbl_title.Text = "儲存設定";
+            // 
+            // btn_Save
+            // 
+            this.btn_Save.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(166)))), ((int)(((byte)(137)))));
+            this.btn_Save.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(166)))), ((int)(((byte)(137)))));
+            this.btn_Save.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Save.Font = new System.Drawing.Font("微軟正黑體", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btn_Save.ForeColor = System.Drawing.Color.White;
+            this.btn_Save.Location = new System.Drawing.Point(548, 465);
+            this.btn_Save.Margin = new System.Windows.Forms.Padding(2);
+            this.btn_Save.Name = "btn_Save";
+            this.btn_Save.Size = new System.Drawing.Size(75, 28);
+            this.btn_Save.TabIndex = 108;
+            this.btn_Save.Text = "儲存";
+            this.btn_Save.UseVisualStyleBackColor = false;
+            this.btn_Save.Click += new System.EventHandler(this.btn_Save_Click);
+            // 
+            // btn_Close
+            // 
+            this.btn_Close.BackColor = System.Drawing.Color.DarkGray;
+            this.btn_Close.FlatAppearance.BorderColor = System.Drawing.Color.DarkGray;
+            this.btn_Close.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Close.Font = new System.Drawing.Font("微軟正黑體", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btn_Close.Location = new System.Drawing.Point(463, 465);
+            this.btn_Close.Margin = new System.Windows.Forms.Padding(2);
+            this.btn_Close.Name = "btn_Close";
+            this.btn_Close.Size = new System.Drawing.Size(75, 28);
+            this.btn_Close.TabIndex = 109;
+            this.btn_Close.Text = "關閉";
+            this.btn_Close.UseVisualStyleBackColor = false;
+            this.btn_Close.Click += new System.EventHandler(this.btn_Close_Click);
+            // 
+            // cbx_save_Image2
+            // 
+            this.cbx_save_Image2.Appearance = System.Windows.Forms.Appearance.Button;
+            this.cbx_save_Image2.AutoSize = true;
+            this.cbx_save_Image2.BackgroundImage = global::Painter2.Properties.Resources.OFF_edited;
+            this.cbx_save_Image2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.cbx_save_Image2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbx_save_Image2.Location = new System.Drawing.Point(261, 192);
+            this.cbx_save_Image2.Name = "cbx_save_Image2";
+            this.cbx_save_Image2.Size = new System.Drawing.Size(65, 34);
+            this.cbx_save_Image2.TabIndex = 111;
+            this.cbx_save_Image2.Tag = "save_Image2";
+            this.cbx_save_Image2.Text = "         ";
+            this.cbx_save_Image2.UseVisualStyleBackColor = true;
+            this.cbx_save_Image2.CheckedChanged += new System.EventHandler(this.cbx_CheckedChanged);
+            // 
+            // lbl_save_Image2
+            // 
+            this.lbl_save_Image2.AutoSize = true;
+            this.lbl_save_Image2.Font = new System.Drawing.Font("微軟正黑體", 12F);
+            this.lbl_save_Image2.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.lbl_save_Image2.Location = new System.Drawing.Point(332, 202);
+            this.lbl_save_Image2.Name = "lbl_save_Image2";
+            this.lbl_save_Image2.Size = new System.Drawing.Size(38, 20);
+            this.lbl_save_Image2.TabIndex = 110;
+            this.lbl_save_Image2.Text = "OFF";
+            // 
+            // cbx_Save_SameImageFormat
+            // 
+            this.cbx_Save_SameImageFormat.Appearance = System.Windows.Forms.Appearance.Button;
+            this.cbx_Save_SameImageFormat.AutoSize = true;
+            this.cbx_Save_SameImageFormat.BackgroundImage = global::Painter2.Properties.Resources.OFF_edited;
+            this.cbx_Save_SameImageFormat.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.cbx_Save_SameImageFormat.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbx_Save_SameImageFormat.Location = new System.Drawing.Point(261, 297);
+            this.cbx_Save_SameImageFormat.Name = "cbx_Save_SameImageFormat";
+            this.cbx_Save_SameImageFormat.Size = new System.Drawing.Size(65, 34);
+            this.cbx_Save_SameImageFormat.TabIndex = 114;
+            this.cbx_Save_SameImageFormat.Tag = "Save_SameImageFormat";
+            this.cbx_Save_SameImageFormat.Text = "         ";
+            this.cbx_Save_SameImageFormat.UseVisualStyleBackColor = true;
+            this.cbx_Save_SameImageFormat.CheckedChanged += new System.EventHandler(this.cbx_CheckedChanged);
+            // 
+            // lbl_Save_SameImageFormat
+            // 
+            this.lbl_Save_SameImageFormat.AutoSize = true;
+            this.lbl_Save_SameImageFormat.Font = new System.Drawing.Font("微軟正黑體", 12F);
+            this.lbl_Save_SameImageFormat.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.lbl_Save_SameImageFormat.Location = new System.Drawing.Point(332, 307);
+            this.lbl_Save_SameImageFormat.Name = "lbl_Save_SameImageFormat";
+            this.lbl_Save_SameImageFormat.Size = new System.Drawing.Size(38, 20);
+            this.lbl_Save_SameImageFormat.TabIndex = 113;
+            this.lbl_Save_SameImageFormat.Text = "OFF";
+            // 
+            // label_Save_SameImageFormat
+            // 
+            this.label_Save_SameImageFormat.AutoSize = true;
+            this.label_Save_SameImageFormat.Font = new System.Drawing.Font("微軟正黑體", 12F);
+            this.label_Save_SameImageFormat.Location = new System.Drawing.Point(10, 305);
+            this.label_Save_SameImageFormat.Name = "label_Save_SameImageFormat";
+            this.label_Save_SameImageFormat.Size = new System.Drawing.Size(249, 20);
+            this.label_Save_SameImageFormat.TabIndex = 112;
+            this.label_Save_SameImageFormat.Text = "儲存影像檔案格式和原始影像相同";
+            // 
+            // cbx_save_label
+            // 
+            this.cbx_save_label.Appearance = System.Windows.Forms.Appearance.Button;
+            this.cbx_save_label.AutoSize = true;
+            this.cbx_save_label.BackgroundImage = global::Painter2.Properties.Resources.OFF_edited;
+            this.cbx_save_label.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.cbx_save_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbx_save_label.Location = new System.Drawing.Point(261, 87);
+            this.cbx_save_label.Name = "cbx_save_label";
+            this.cbx_save_label.Size = new System.Drawing.Size(65, 34);
+            this.cbx_save_label.TabIndex = 119;
+            this.cbx_save_label.Tag = "save_label";
+            this.cbx_save_label.Text = "         ";
+            this.cbx_save_label.UseVisualStyleBackColor = true;
+            this.cbx_save_label.CheckedChanged += new System.EventHandler(this.cbx_CheckedChanged);
+            // 
+            // lbl_save_label
+            // 
+            this.lbl_save_label.AutoSize = true;
+            this.lbl_save_label.Font = new System.Drawing.Font("微軟正黑體", 12F);
+            this.lbl_save_label.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.lbl_save_label.Location = new System.Drawing.Point(332, 97);
+            this.lbl_save_label.Name = "lbl_save_label";
+            this.lbl_save_label.Size = new System.Drawing.Size(38, 20);
+            this.lbl_save_label.TabIndex = 118;
+            this.lbl_save_label.Text = "OFF";
+            // 
+            // label_ImageFormat_Save
+            // 
+            this.label_ImageFormat_Save.AutoSize = true;
+            this.label_ImageFormat_Save.Font = new System.Drawing.Font("微軟正黑體", 12F);
+            this.label_ImageFormat_Save.Location = new System.Drawing.Point(381, 305);
+            this.label_ImageFormat_Save.Name = "label_ImageFormat_Save";
+            this.label_ImageFormat_Save.Size = new System.Drawing.Size(137, 20);
+            this.label_ImageFormat_Save.TabIndex = 125;
+            this.label_ImageFormat_Save.Text = "儲存影像檔案格式";
+            // 
+            // label_ImageFormat_Load
+            // 
+            this.label_ImageFormat_Load.AutoSize = true;
+            this.label_ImageFormat_Load.Font = new System.Drawing.Font("微軟正黑體", 12F);
+            this.label_ImageFormat_Load.Location = new System.Drawing.Point(381, 270);
+            this.label_ImageFormat_Load.Name = "label_ImageFormat_Load";
+            this.label_ImageFormat_Load.Size = new System.Drawing.Size(137, 20);
+            this.label_ImageFormat_Load.TabIndex = 124;
+            this.label_ImageFormat_Load.Text = "載入影像檔案格式";
+            // 
+            // label_FileName_Image3
+            // 
+            this.label_FileName_Image3.AutoSize = true;
+            this.label_FileName_Image3.Enabled = false;
+            this.label_FileName_Image3.Font = new System.Drawing.Font("微軟正黑體", 12F);
+            this.label_FileName_Image3.Location = new System.Drawing.Point(381, 235);
+            this.label_FileName_Image3.Name = "label_FileName_Image3";
+            this.label_FileName_Image3.Size = new System.Drawing.Size(89, 20);
